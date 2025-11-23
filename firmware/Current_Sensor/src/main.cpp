29a7140ba359ace0d27e21e517ce926daba3f123
Kevin Gosselin <kgossel@ncsu.edu>
2025-11-23 11:07:16 -0500

    Troubleshooted ACS712

--- firmware/Current_Sensor/src/main.cpp
+++ firmware/Current_Sensor/src/main.cpp
@@ -1,25 +1,25 @@
 #include <Arduino.h>
 
-#define sensorPin 34
+#define sensorPin 35
 
 const float vRef = 3.3;
 const float adcResolution = 4095;
 const float zeroPoint = 1.67;
-const float dividerRatio = 0.665;
+const float dividerRatio = 0.6667; // Divider Ratio for a 1k + 2k Ohm V. Div.
 const float sensitivity = 0.100 * dividerRatio;
 
 void sensorRead(void *parameter){
   for(;;){
-    float avgCurrent = 0;
-    float sumCurrent = 0;
+    uint32_t sumPin_mV = 0;
     for (int i = 0; i <= 99; i++){
-    int adcValue = analogRead(sensorPin);
-    float pinVoltage = (adcValue/adcResolution)*vRef;
-    float current = (pinVoltage - zeroPoint)/sensitivity;
-    sumCurrent = current + sumCurrent;
+      uint32_t pin_mV = analogReadMilliVolts(sensorPin);
+      sumPin_mV = sumPin_mV + pin_mV;
     }
-    avgCurrent = sumCurrent/100;
-    Serial.println(avgCurrent);
+    float avgPin_mV = sumPin_mV/100;
+    float pinVoltage = avgPin_mV/1000;
+    float current = (pinVoltage - zeroPoint)/sensitivity;
+
+    Serial.println(current);
     vTaskDelay(100/portTICK_PERIOD_MS);
   }
 }
@@ -27,7 +27,7 @@ void sensorRead(void *parameter){
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  analogSetAttenuation(ADC_11db);
+  analogSetPinAttenuation(sensorPin, ADC_11db);
   pinMode(sensorPin, INPUT);
   xTaskCreatePinnedToCore(sensorRead, "sensorRead", 3000, NULL, 1, NULL, 1);
 }
