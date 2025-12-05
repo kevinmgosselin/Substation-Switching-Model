7709bbb8a9b097d894e3ea5bd7399936557d9abb
Kevin Gosselin <kgossel@ncsu.edu>
2025-12-05 17:23:19 -0500

    Introduced current sensor to Servo_2switches

--- firmware/Servo_2switches/src/main.cpp
+++ firmware/Servo_2switches/src/main.cpp
@@ -2,9 +2,18 @@
 #include <ESP32Servo.h>
 
 #define switchOpenPin 13
-#define switchClosePin 2
+#define switchClosePin 4
+#define sensorPin 35
 
-static const int servoPin = 18;
+const float vRef = 3.3;
+const float adcResolution = 4095;
+const float zeroPoint = 1.67;
+const float dividerRatio = 0.6667; // Divider Ratio for a 1k + 2k Ohm V. Div.
+const float sensitivity = 0.100 * dividerRatio;
+
+static const int servoPin = 5;
+
+float current = 0;
 
 Servo servo;
 
@@ -15,31 +24,51 @@ typedef enum {
 
 State sysState = OPEN;
 
+void sensorRead(void *parameter){
+  for(;;){
+    uint32_t sumPin_mV = 0;
+    for (int i = 0; i <= 99; i++){
+      uint32_t pin_mV = analogReadMilliVolts(sensorPin);
+      sumPin_mV = sumPin_mV + pin_mV;
+    }
+    float avgPin_mV = sumPin_mV/100;
+    float pinVoltage = avgPin_mV/1000;
+    current = (pinVoltage - zeroPoint)/sensitivity;
+
+    Serial.printf("Current: %f\n",current);
+    vTaskDelay(100/portTICK_PERIOD_MS);
+  }
+}
+
 void servoTask(void *parameter){
   int posDegrees = 0;
   for(;;){
     int switchStateOpen = digitalRead(switchOpenPin);
     int switchStateClose = digitalRead(switchClosePin);
-    if (switchStateOpen == 0 && sysState == OPEN){
+    if (switchStateOpen == 1 && sysState == OPEN && posDegrees < 180){
       posDegrees++;
     }
-    if (switchStateClose == 0 && sysState == CLOSE){
+    if (switchStateClose == 1 && sysState == CLOSE && posDegrees > 0){
       posDegrees--;
     }
     servo.write(posDegrees);
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    Serial.printf("OpenSwitch: %u", switchStateOpen);
-    Serial.printf("CloseSwitch: %u", switchStateClose);
+    vTaskDelay(10/portTICK_PERIOD_MS);
+    Serial.printf("OpenSwitch: %u\n", switchStateOpen);
+    Serial.printf("CloseSwitch: %u\n", switchStateClose);
     Serial.println(posDegrees);
   }
 }
 
 void stateSwitcher(void *parameter){
   for(;;){
-    sysState = OPEN;
-    vTaskDelay(10000/portTICK_PERIOD_MS);
-    sysState = CLOSE;
-    vTaskDelay(10000/portTICK_PERIOD_MS);
+    if (current > 0.3){ // For 5v and 10 ohm resistor
+      sysState = OPEN;
+    }
+    else{
+      sysState = CLOSE;
+    }
+    vTaskDelay(100/portTICK_PERIOD_MS);
+    Serial.printf("System State: %s\n", sysState);
   }
 }
 
@@ -47,11 +76,14 @@ void setup() {
   Serial.begin(115200);
   servo.attach(servoPin);
 
+  analogSetPinAttenuation(sensorPin, ADC_11db);
+  pinMode(sensorPin, INPUT);
   pinMode(switchOpenPin, INPUT_PULLUP);
   pinMode(switchClosePin, INPUT_PULLUP);
 
   xTaskCreatePinnedToCore(servoTask, "servoTask", 3000, NULL, 1, NULL, 1);
   xTaskCreatePinnedToCore(stateSwitcher, "stateSwitcher", 3000, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(sensorRead, "sensorRead", 3000, NULL, 1, NULL, 1);
 }
 
 void loop() {
